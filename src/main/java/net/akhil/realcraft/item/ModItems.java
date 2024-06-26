fdb89e5a420d7cb083f1319b07ac202af24bf44c
Akhil2014-j <anushakumari1206@gmail.com>
2024-06-26 17:27:57 -0400

    Bug fix

--- src/main/java/net/akhil/realcraft/item/ModItems.java
+++ src/main/java/net/akhil/realcraft/item/ModItems.java
@@ -1,13 +1,23 @@
 package net.akhil.realcraft.item;
 
+import net.fabricmc.fabric.api.item.v1.*;
 import net.akhil.realcraft.RealCraft;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-public class ModItems {
+public class ModItems{
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
 
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.tryParse(RealCraft.MOD_ID, name), item);
@@ -15,5 +25,7 @@ public class ModItems {
 
     public static void registerModItems() {
         RealCraft.LOGGER.info("Registering Mod Items for " + RealCraft.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup)
     }
 }
