cafd4c308d54312fff2577c851c76fb751985089
Akhil2014-j <anushakumari1206@gmail.com>
2024-06-25 23:09:48 -0400

    First

--- src/main/java/net/akhil/realcraft/RealCraft.java
+++ src/main/java/net/akhil/realcraft/RealCraft.java
@@ -1,5 +1,6 @@
 package net.akhil.realcraft;
 
+import net.akhil.realcraft.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class RealCraft implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/akhil/realcraft/item/ModItems.java
+++ src/main/java/net/akhil/realcraft/item/ModItems.java
@@ -1,4 +1,19 @@
 package net.akhil.realcraft.item;
 
+import net.akhil.realcraft.RealCraft;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(RealCraft.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        RealCraft.LOGGER.info("Registering Mod Items for " + RealCraft.MOD_ID);
+    }
 }
