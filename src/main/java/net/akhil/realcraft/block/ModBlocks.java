a4d93aa81d09ff42aab8f60dc21cd3af4783469e
Akhil2014-j <anushakumari1206@gmail.com>
2024-08-01 14:00:46 -0400

    Version 1.0.5 TEST BUILD 1!
    Added 2 new blocks
    Added 1 special block(Unlit Torch)
    
    Compatible with Fabric 0.100.4+1.21 and Minecraft Java 1.21!
    
    This is the last version of RealCraft to exist on Fabric! RealCraft will soon support only Forge! Don't worry you can still access this version anytime, it just won't be the latest one!

--- src/main/java/net/akhil/realcraft/block/ModBlocks.java
+++ src/main/java/net/akhil/realcraft/block/ModBlocks.java
@@ -3,8 +3,12 @@ package net.akhil.realcraft.block;
 import net.akhil.realcraft.RealCraft;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.TorchBlock;
+import net.minecraft.block.WallTorchBlock;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -24,6 +28,12 @@ public class ModBlocks {
 
     public static final Block RUBY_BLOCK = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(5.0f, 6.0f)), "ruby_block", true);
     public static final Block SAPPHIRE_BLOCK = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(5.0f, 6.0f)), "sapphire_block", true);
+    public static final Block RUBY_ORE = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE)), "ruby_ore", true);
+    public static final Block SAPPHIRE_ORE = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE)), "sapphire_ore", true);
+    public static final Block DEEPSLATE_RUBY_ORE = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.DEEPSLATE)), "deepslate_ruby_ore", true);
+    public static final Block DEEPSLATE_SAPPHIRE_ORE = register(new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.DEEPSLATE)), "deepslate_sapphire_ore", true);
+    public static final Block BURNT_OUT_TORCH = register(new TorchBlock(ParticleTypes.SMOKE, AbstractBlock.Settings.create().noCollision().breakInstantly().luminance((state) -> 0).sounds(BlockSoundGroup.WOOD).pistonBehavior(PistonBehavior.DESTROY)), "burnt_out_torch", true);
+    public static final Block BURNT_OUT_WALL_TORCH = register(new WallTorchBlock(ParticleTypes.SMOKE, AbstractBlock.Settings.create().noCollision().breakInstantly().luminance((state) -> 0).sounds(BlockSoundGroup.WOOD).dropsLike(BURNT_OUT_TORCH).pistonBehavior(PistonBehavior.DESTROY)), "burnt_out_wall_torch", true);
 
     public static void initialize() {}
 }
--- src/main/java/net/akhil/realcraft/item/ModItems.java
+++ src/main/java/net/akhil/realcraft/item/ModItems.java
@@ -2,30 +2,59 @@ package net.akhil.realcraft.item;
 
 
 import net.akhil.realcraft.RealCraft;
+import net.akhil.realcraft.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 
 
 public class ModItems{
+
+    public static final RegistryKey<ItemGroup> MOD_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(RealCraft.MOD_ID, "item_group"));
+    public static final ItemGroup REALCRAFT = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.RUBY))
+            .displayName(Text.translatable("itemGroup.realcraft"))
+            .build();
+
     public static Item register(Item item, String id) {
+
+
+
         Identifier itemID = Identifier.of(RealCraft.MOD_ID, id);
 
         Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        Registry.register(Registries.ITEM_GROUP, MOD_ITEM_GROUP_KEY, REALCRAFT);
 
         return registeredItem;
     }
 
     public static final Item RUBY = register(new Item(new Item.Settings()), "ruby");
     public static final Item SAPPHIRE = register(new Item(new Item.Settings()), "sapphire");
+    public static final Item BURNT_OUT_TORCH = register((BlockItem) (new VerticallyAttachableBlockItem(ModBlocks.BURNT_OUT_TORCH, ModBlocks.BURNT_OUT_WALL_TORCH,  new Item.Settings(), Direction.DOWN)), "burnt_out_torch_item");
 
 
     public static void initialize() {
         // Items in Item Groups
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> itemGroup.add(ModItems.RUBY));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> itemGroup.add(ModItems.SAPPHIRE));
+
+        ItemGroupEvents.modifyEntriesEvent(MOD_ITEM_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(ModItems.RUBY);
+            itemGroup.add(ModItems.SAPPHIRE);
+            itemGroup.add(ModBlocks.RUBY_BLOCK);
+            itemGroup.add(ModBlocks.SAPPHIRE_BLOCK);
+            itemGroup.add(ModBlocks.RUBY_ORE);
+            itemGroup.add(ModBlocks.SAPPHIRE_ORE);
+            itemGroup.add(ModBlocks.BURNT_OUT_TORCH);
+        });
     }
+
+
 }
